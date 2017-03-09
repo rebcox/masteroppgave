048d527f6e09c22b258384d417d5f2f572ba415a
Rebecca Cox <rebecclc@stud.ntnu.no>
2017-03-09 15:34:10 +0100

    Env maker draw obstacle

--- environment_maker/main.cpp
+++ environment_maker/main.cpp
@@ -3,61 +3,104 @@
 #include <fstream>
 #include <iostream>
 
-struct ButtonClick
-{
-  int x = -1;
-  int y = -1;
-};
+
 struct PointsAndImage
 {
-  std::vector<ButtonClick> points;
+  std::vector<std::vector<cv::Point>> obstacles;
   cv::Mat image;
+  bool obstacle_submitted = true;
 };
+
+bool is_listed_clockwise(const std::vector<cv::Point> &points)
+{
+  //Shoelace formula
+  int n_points = points.size();
+  int sum = 0;
+
+  if (n_points>0)
+  {
+    sum += (points[0].x - points.back().x)*(points[0].y - points.back().y);
+    for (int i = 0; i < n_points-1; ++i)
+    {
+      sum += (points[i+1].x - points[i].x) * (points[i+1].y + points[i].y);
+    }
+  }
+  if (sum > 0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+bool drawObstacle(std::vector<cv::Point> &obstacle, cv::Mat &image)
+{
+  if (obstacle.size() < 3)
+  {
+    std::cout << "Error: Obstacle is a point or a line" << std::endl;
+    return false;
+  } 
+  
+  cv::Scalar color(0,0,255,255);
+
+  line(image, obstacle.back(), obstacle[0], color);
+
+  for (int i = 0; i < obstacle.size()-1; ++i)
+  {
+    line(image, obstacle[i], obstacle[i+1], color);
+  }
+
+  imshow("Map", image);
+  return true;
+}
 
 void callBackFunc(int event, int x, int y, int flags, void* userdata)
 {
-
- // std::vector<ButtonClick> *points_pointer = static_cast<std::vector<ButtonClick>*>(userdata);
-  ButtonClick bc;
   PointsAndImage *points_and_image = static_cast<PointsAndImage*>(userdata);
 
   if  ( event == cv::EVENT_LBUTTONDOWN )
   {
-    std::cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << std::endl;
-    //bc.x = x; bc.y = y;
-    //points_pointer->push_back(bc);
-  
-    bc.x = x; bc.y = y;
-    points_and_image->points.push_back(bc);
-    
+    std::cout << "Point added at position (" << x << ", " << y << ")" << std::endl;
     cv::Point pt(x,y);
+
+    if (points_and_image->obstacle_submitted)
+    {
+      std::vector<cv::Point> points;
+      points.push_back(pt);
+      points_and_image->obstacles.push_back(points);
+      points_and_image->obstacle_submitted = false;
+    }
+    else
+    {
+      points_and_image->obstacles.back().push_back(pt);
+    }
+    
     cv::Scalar color(0,0,255,255);
     cv::circle(points_and_image->image, pt, 2, color);
     imshow("Map", points_and_image->image);
   }
+
   else if (event == cv::EVENT_MBUTTONDOWN )
   {
-    std::cout << "Middle button of the mouse is clicked. Obstacle added to environment" << std::endl;
-
-    points_and_image->points.push_back(bc);    
+    drawObstacle(points_and_image->obstacles.back(), points_and_image->image);
+    points_and_image->obstacle_submitted = true;
+    std::cout << "Obstacle added to environment" << std::endl;    
   }
-
 }
 
-void write_environment_to_file(const std::vector<ButtonClick> &points)
+void write_environment_to_file(const std::vector<std::vector<cv::Point>> &obstacles)
 {
   std::ofstream file_output;
   file_output.open ("environment.txt");
-  for (int i = 0; i < points.size(); i++)
+  for (int i = 0; i < obstacles.size(); i++)
   {
-    if (points[i].x == -1 && points[i].y == -1)
+    for (int j = 0; j < obstacles[i].size(); ++j)
     {
-      file_output << "\n";
+      file_output << obstacles[i][j].x << "," << obstacles[i][j].y << "\n";
     }
-    else
-    {
-      file_output << points[i].x << "," << points[i].y << "\n";
-    }
+    file_output << "\n";
   }
 
   file_output.close();
@@ -65,9 +108,7 @@ void write_environment_to_file(const std::vector<ButtonClick> &points)
 
 int main(int argc, char** argv)
 {
-  // Read image from file 
   cv::Mat img = cv::imread("map.jpg");
-  //std::cout << img.size();
 
   if ( img.empty() ) 
   { 
@@ -83,8 +124,33 @@ int main(int argc, char** argv)
 
   cv::setMouseCallback("Map", callBackFunc, &points_and_image);
 
-  cv::waitKey(0);
+  while(1)
+  {
+    char key = (char)cv::waitKey();
+    if(key==27) break;
+    else if (key==13)
+    {
+      //First obstacle (outer boundary) must be listet counter clockwise
+      if (points_and_image.obstacles.size()==1)
+      {
+        if (is_listed_clockwise(points_and_image.obstacles.back()))
+        {
+          std::reverse(points_and_image.obstacles.back().begin(), points_and_image.obstacles.back().end());
+        }
+      }//All other obstacles must be listed clockwise
+      else if(!is_listed_clockwise(points_and_image.obstacles.back()))
+      {
+        std::reverse(points_and_image.obstacles.back().begin(), points_and_image.obstacles.back().end());
+      }
 
-  write_environment_to_file(points_and_image.points);
+      drawObstacle(points_and_image.obstacles.back(), points_and_image.image);
+      points_and_image.obstacle_submitted = true;
+      std::cout << "Obstacle added to environment" << std::endl;
+    }
+  }             
+
+  write_environment_to_file(points_and_image.obstacles);
+  std::cout << "yeess" << std::endl;
+
   return 0;
 }
